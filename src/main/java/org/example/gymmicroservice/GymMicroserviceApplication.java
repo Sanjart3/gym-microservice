5c43f877e62805daab4ad69d29ec929f48b5ca23
Sanjar Totliboyev <sanjartotliboyev@gmail.com>
2024-10-25 16:02:08 +0500

    Project initialization

---
+++ src/main/java/org/example/gymmicroservice/GymMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package org.example.gymmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gymmicroservice/GymMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gymmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
