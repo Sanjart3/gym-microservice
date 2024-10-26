fcce4bf94873d199dc63b933456bab8e28b20db7
Sanjar Totliboyev <sanjartotliboyev@gmail.com>
2024-10-26 13:56:15 +0500

    Training event endpoints and logic have been implemented

--- training-service/src/main/java/org/example/trainingservice/services/impl/TrainingEventServiceImpl.java
+++ training-service/src/main/java/org/example/trainingservice/services/impl/TrainingEventServiceImpl.java
@@ -1,12 +1,20 @@
 package org.example.trainingservice.services.impl;
 
 import lombok.extern.slf4j.Slf4j;
+import org.example.trainingservice.dto.TrainerSummaryDto;
 import org.example.trainingservice.dto.TrainingEventsDto;
 import org.example.trainingservice.entities.TrainingEvents;
+import org.example.trainingservice.enums.ActionType;
 import org.example.trainingservice.repositories.TrainingEventRepository;
 import org.example.trainingservice.services.TrainingService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @Slf4j
@@ -23,13 +31,39 @@ public class TrainingEventServiceImpl implements TrainingService {
         log.debug("Saving training event: {}", trainingEventsDto);
         TrainingEvents trainingEvents = modelMapper.map(trainingEventsDto, TrainingEvents.class);
 
-        trainingEventRepository.save(trainingEvents);
-        log.debug("Saved training event: {}", trainingEventsDto);
+        TrainingEvents savedTrainingEvent = trainingEventRepository.save(trainingEvents);
+        log.debug("Saved training event with ID: {}", savedTrainingEvent.getId());
     }
 
     @Override
-    public void deleteTrainingEvent(TrainingEventsDto trainingEventsDto) {
-        log.debug("Deleting training event: {}", trainingEventsDto);
+    public List<TrainerSummaryDto> countTrainerSummary() {
+        log.debug("Counting trainers' summary");
+        List<TrainingEvents> trainingEvents = trainingEventRepository.findAllByAction(ActionType.ADD);
+        Map<String, TrainerSummaryDto> trainerSummaries = new HashMap<>();
+        for (TrainingEvents trainingEvent : trainingEvents) {
+            TrainerSummaryDto trainerSummary = trainerSummaries.getOrDefault(trainingEvent.getTrainerUsername(), new TrainerSummaryDto(
+                    trainingEvent.getTrainerUsername(),
+                    trainingEvent.getTrainerFirstName(),
+                    trainingEvent.getTrainerLastName(),
+                    trainingEvent.getIsActive()
+            ));
+            Integer year = getYear(trainingEvent);
+            Integer month = getMonth(trainingEvent);
 
+            trainerSummary.addTrainingTime(year, month, trainingEvent.getTrainingDuration()); // Adding Duration
+            trainerSummaries.put(trainingEvent.getTrainerUsername(), trainerSummary);
+        }
+
+        return new ArrayList<>(trainerSummaries.values());
+    }
+
+    public Integer getYear(TrainingEvents trainingEvent) {
+        LocalDate date = trainingEvent.getTrainingDate();
+        return date.getYear();
+    }
+
+    public Integer getMonth(TrainingEvents trainingEvent) {
+        LocalDate date = trainingEvent.getTrainingDate();
+        return date.getMonthValue();
     }
 }
