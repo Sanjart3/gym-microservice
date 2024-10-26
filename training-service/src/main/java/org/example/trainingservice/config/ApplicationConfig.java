992cc9b504e2a50d56fc7de50c98f7edb2a80086
Sanjar Totliboyev <sanjartotliboyev@gmail.com>
2024-10-26 16:58:13 +0500

    Eureka Server implemented

--- training-service/src/main/java/org/example/trainingservice/config/ApplicationConfig.java
+++ training-service/src/main/java/org/example/trainingservice/config/ApplicationConfig.java
@@ -1,7 +1,7 @@
 package org.example.trainingservice.config;
 
 import lombok.RequiredArgsConstructor;
-import org.example.trainingservice.filter.Slf4jMDCFilter;
+import org.example.trainingservice.filter.SLF4JMDCFilter;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -9,11 +9,11 @@ import org.springframework.context.annotation.Bean;
 @Configurable
 @RequiredArgsConstructor
 public class ApplicationConfig {
-    private final Slf4jMDCFilter slf4jMDCFilter;
+    private final SLF4JMDCFilter slf4jMDCFilter;
 
     @Bean
-    public FilterRegistrationBean<Slf4jMDCFilter> servletRegistrationBean() {
-        final FilterRegistrationBean<Slf4jMDCFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+    public FilterRegistrationBean<SLF4JMDCFilter> servletRegistrationBean() {
+        final FilterRegistrationBean<SLF4JMDCFilter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(slf4jMDCFilter);
         filterRegistrationBean.setOrder(2);
         return filterRegistrationBean;
